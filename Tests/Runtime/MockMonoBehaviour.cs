222d6ccc7b51cc1afba388d43c63d5aaf71b12a4
jeffbert <jeffbert000@gmail.com>
2022-11-09 21:03:34 -0500

    Hide MockMonoBehaviour from component menu

--- Tests/Runtime/MockMonoBehaviour.cs
+++ Tests/Runtime/MockMonoBehaviour.cs
@@ -5,6 +5,7 @@ namespace Bert.Pool.Tests
     /// <summary>
     /// Empty <see cref="MonoBehaviour"/> class used for testing.
     /// </summary>
+    [AddComponentMenu("")]
     internal sealed class MockMonoBehaviour : MonoBehaviour
     {
     }
